8e03a6d21be5303b09cd18e9bc8d041365a39f66
savichev-aa <savichev.pp@gmail.com>
2023-07-19 12:05:08 +0400

    функция 2

--- js/functions.js
+++ js/functions.js
@@ -3,9 +3,35 @@
 function stringLenght(string, lenght) {
   if (string.length <= lenght) {
     return true;
-  } else {
-    return false;
   }
+    return false;
 }
 
 // Вторая функция
+
+function polidrom (word) {
+  // Первый способ
+
+  // let word2 = word.split('');
+  // word2 = word2.reverse();
+  // word2 = word2.join('');
+
+  // Второй способ
+  let word2 = [];
+  for (i = 0; i < word.length; i++) {
+    word2[i] = word[i]
+}
+  word2 = word2.reverse();
+  word2 = word2.join('');
+
+ if (word === word2) {
+  return true
+ }
+  return false
+}
+
+console.log(polidrom('топот'))
+
+// Третья функция
+
+
